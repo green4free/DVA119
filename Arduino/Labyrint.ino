af09d8a37b56187f5ec1f386de9d8202bc804d89
green4free <ninjalostinsnow@gmail.com>
2015-09-25 00:00:33 +0200

    Added the initial arduino sketches

---
+++ Arduino/Labyrint.ino
@@ -0,0 +1,39 @@
+#include <Adafruit_MotorShield.h>
+#include <lenlib.h> 
+#include <Servo.h> 
+#include <Wire.h> 
+
+int R0Val = 0;
+Servo Servo1;
+mySensors Sensors; // create Sensors object
+myMotors Motors;  // create Motors object
+
+  void setup() 
+  { 
+    Servo1.attach(10);
+    Motors.beginMotors();   // start motors
+    Sensors.beginSensors(); // start sensors
+  }
+
+  void loop() 
+  {
+    Servo1.write(120);
+    Motors.runMotor(1, FORWARD, 50);
+    Motors.runMotor(2, FORWARD, 50);
+    delay(1000);
+    Motors.runMotor(1, FORWARD, 0);
+    Motors.runMotor(2, FORWARD, 0);
+    Plocka_Boll();
+    Motors.runMotor(1, BACKWARD, 50);
+    Motors.runMotor(2, BACKWARD, 50);
+    delay(1000);
+    Motors.runMotor(1, BACKWARD, 0);
+    Motors.runMotor(2, BACKWARD, 0);
+  }
+  void Plocka_Boll ()
+  {
+    delay(1000);
+    Servo1.write(0);
+    delay(1000);
+  }
+
---
+++ Arduino/Line_following.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_MotorShield.h>
+#include <lenlib.h> 
+#include <Servo.h> 
+#include <Wire.h> 
+
+int sen[3] = {0,0,0};
+mySensors Sensors; // create Sensors object
+myMotors Motors;  // create Motors object
+volatile int state = LOW;
+
+void setup() 
+{  
+  Motors.beginMotors();   // start motors
+  Sensors.beginSensors(); // start sensors
+}
+
+void loop() 
+{
+  
+  sen[0] = Sensors.readReflect0(); sen[1] = Sensors.readReflect1(); sen[2] = Sensors.readReflect2(); // Read digital value of reflect sensor 0
+
+  if (sen[0] == 0 && sen[1] == 1 && sen[2] == 0)
+  {
+      Motors.runMotor(1, FORWARD, 80); // Run motor 1 & 2 forward at 0 velocity(Stop)
+      Motors.runMotor(2, FORWARD, 80);
+  }else if(sen[0] == 1 && sen[1] == 0 && sen[2] == 0)
+  {
+      Motors.runMotor(1, FORWARD, 100); // Run motor 1 & 2 forward at 0 velocity(Stop)
+      Motors.runMotor(2, BACKWARD, 60);
+  }else if(sen[0] == 0 && sen[1] == 0 && sen[2] == 1)
+  {
+      Motors.runMotor(2, FORWARD, 100); // Run motor 1 & 2 forward at 0 velocity(Stop)
+      Motors.runMotor(1, BACKWARD, 60);
+  }else if(sen[0] == 0 && sen[1] == 1 && sen[2] == 1)
+  {
+      Motors.runMotor(2, FORWARD, 80); // Run motor 1 & 2 forward at 0 velocity(Stop)
+      Motors.runMotor(1, FORWARD, 65);
+  }else if(sen[0] == 1 && sen[1] == 1 && sen[2] == 0)
+  {
+      Motors.runMotor(1, FORWARD, 80); // Run motor 1 & 2 forward at 0 velocity(Stop)
+      Motors.runMotor(2, FORWARD, 65);
+  }
+  else
+  {
+      delay(100);
+      if(sen[0] == 1 || sen[1] == 1 || sen[2] == 1){
+          Motors.runMotor(1, FORWARD, 0); // Run motor 1 & 2 forward at 100 velocity
+          Motors.runMotor(2, FORWARD, 0);
+      }else{
+          Motors.runMotor(1, BACKWARD, 50); // Run motor 1 & 2 forward at 100 velocity
+          Motors.runMotor(2, BACKWARD, 50);
+      }
+    }
+}
+
